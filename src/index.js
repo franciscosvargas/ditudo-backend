de6884b59a38ec4df94b6f0f453fed857a5b766c
Francisco <falisson.sv@gmail.com>
2019-08-20 15:56:17 -0300

    Muda a configuração manual do cors para o modulo Cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const openRoutes = require('./routes/open')
@@ -8,16 +9,9 @@ const app = express()
 
 mongoose.connect('mongodb+srv://backend:backendawon@principal-sv9tu.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true})
 
+app.use(cors)
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-})
 
 app.use(openRoutes)
 app.use(closedRoutes)
